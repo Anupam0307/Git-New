1cc97378ee413bbab7468577b5f9d93a39c20d20
Anupam <AbhishekPC@DESKTOP-9OAQQQN>
2019-08-16 21:14:49 +0530

    New

---
+++ Newgit/src/Git11.java
@@ -0,0 +1,19 @@
+
+public class Git11 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is the new project");
+	     int a=10;
+	     int b=20;
+	     int c;
+	      c=a;
+	      a=b;
+	      b=c;
+	      
+	     
+	     System.out.println("The value of a is "+a);
+	     System.out.println("The value of b is "+b);
+	}
+
+}
